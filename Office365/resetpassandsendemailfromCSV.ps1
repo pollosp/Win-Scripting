9e48fca3d803d889bde0780aba3b8cf5d8c472a6
pollosp <pollo.es.pollo@gmail.com>
2014-05-12 12:18:26 +0200

    Added new script
    #Reset passwords from CSV and send email with password with a temporary
    password

---
+++ Office365/resetpassandsendemailfromCSV.ps1
@@ -0,0 +1,31 @@
+﻿#Reset passwords from CSV and send email with password with a temporary password
+# Requires MSolService powershell module http://supertekboy.com/2014/02/20/office-365-how-to-connect-with-powershell/ 
+
+Import-Module MSOnline
+$super_creds=Get-Credential
+Connect-MsolService –Credential $super_creds
+
+ function sendemailpass ($sendto,$upn,$newpass){
+
+     Write-Host "Sending Email"
+
+     $smtpServer = "weblin01"
+     $msg = new-object Net.Mail.MailMessage
+     $smtp = new-object Net.Mail.SmtpClient($smtpServer)
+     $msg.From = "administrador@XXX.es"
+     $msg.ReplyTo = "administrador@XXX.es"
+     $msg.To.Add($sendto)
+     $msg.subject = "Tu cuenta es "+$upn
+     $msg.body = "Tu pasword para la cuenta es "+$newpass
+     $smtp.Send($msg)
+     $msg.Dispose()
+  
+}
+
+
+#CSV HEADER upn,newpass,sendto
+import-csv c:\tmp\emails.txt | ForEach-Object{
+#write-host $_.upn " "  $_.newpass " " $_.sendto
+Set-MsolUserPassword –UserPrincipalName $_.upn  –NewPassword $_.newpass -ForceChangePassword $True
+sendemailpass $_.sendto  $_.upn $_.newpass
+}
