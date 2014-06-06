5cf9d787149bddd351925edd16f1bfeb7b6e5981
pollosp <pollo.es.pollo@gmail.com>
2014-06-06 14:48:16 +0200

    New windows script
    Get path with user permissions when user lastlogon is one more than one
    year

---
+++ Folders-Scritpts/Getpathuserpermissions.ps1
@@ -0,0 +1,24 @@
+﻿#Get paths with user permissions when user lastlogon is one more than one year. 
+
+Get-ADUser -filter * -Properties *  | select SAMAccountName, @{N='LastLogon'; E={[DateTime]::FromFileTime($_.LastLogon)}} | where-object {$_.LastLogon -lt (get-date).AddDays(-365)} | sort lastlogon | ForEach-Object {
+
+write-host $_.SAMAccountName
+$files = @()
+GCI "c:\tmp" -recurse | 
+    ForEach-Object { 
+        $fname = $_.FullName
+        if ($fname -notlike "*_vti*") {
+            $acl = Get-Acl $($fname)
+            foreach ($e in $acl.Access) {
+                If (
+                     $e.IdentityReference -match  $_.SAMAccountName) 
+                {
+                    $files += $fname
+                    break
+                }
+            }
+        }
+    }
+$files 
+
+}
